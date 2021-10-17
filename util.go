9e77cf311d3963ebde050cdf06e4f4a2cae633f4
Aidan Woods <aidantwoods@gmail.com>
2021-10-17 23:34:36 +0100

    Pae

--- util.go
+++ util.go
@@ -1,7 +1,23 @@
 package paseto
 
-func pae(pieces ...[]byte) []byte {
+import (
+	"bytes"
+	"encoding/binary"
+)
 
+// Pae Pre Auth Encode
+func Pae(pieces ...[]byte) []byte {
+	// MSB should be zero
+	count := int64(len(pieces))
+	buffer := &bytes.Buffer{}
+
+	binary.Write(buffer, binary.LittleEndian, count)
+
+	for i := range pieces {
+		// MSB should be zero
+		binary.Write(buffer, binary.LittleEndian, int64(len(pieces[i])))
+		buffer.Write(pieces[i])
+	}
+
+	return buffer.Bytes()
 }
-
-func le64()
