fa93e3d14bf76100884599af0b1abf803f8bac07
Aidan Woods <aidantwoods@gmail.com>
2021-10-18 16:04:59 +0100

    Util for generic hash and base64url encoding

--- util.go
+++ util.go
@@ -2,16 +2,19 @@ package paseto
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/binary"
+	"hash"
+
+	"golang.org/x/crypto/blake2b"
 )
 
 // Pae Pre Auth Encode
-func Pae(pieces ...[]byte) []byte {
-	// MSB should be zero
-	count := int64(len(pieces))
+func pae(pieces ...[]byte) []byte {
 	buffer := &bytes.Buffer{}
 
-	binary.Write(buffer, binary.LittleEndian, count)
+	// MSB should be zero
+	binary.Write(buffer, binary.LittleEndian, int64(len(pieces)))
 
 	for i := range pieces {
 		// MSB should be zero
@@ -20,4 +23,44 @@ func Pae(pieces ...[]byte) []byte {
 	}
 
 	return buffer.Bytes()
+}
+
+func b64urlEncode(bytes []byte) string {
+	encoded := make([]byte, base64.RawURLEncoding.EncodedLen(len(bytes)))
+
+	base64.RawURLEncoding.Encode(encoded, bytes)
+
+	return string(encoded)
+}
+
+func b64urlDecode(encoded string) ([]byte, error) {
+	var bytes []byte
+	var err error
+
+	if bytes, err = base64.RawURLEncoding.DecodeString(encoded); err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
+}
+
+func genericHash(in, out, key []byte, outputLength int) {
+	var blake hash.Hash
+	var err error
+
+	if len(out) != outputLength {
+		panic("Insufficient out length")
+	}
+
+	if blake, err = blake2b.New(outputLength, key); err != nil {
+		panic(err)
+	}
+
+	if _, err = blake.Write(in); err != nil {
+		panic(err)
+	}
+
+	tmp := blake.Sum(nil)
+
+	copy(out, tmp)
 }
