1db145a64da1e392e1f95fe7b39183c55b9296d6
Aidan Woods <git@public.aidanwoods.com>
2022-02-27 19:31:24 +0000

    Add parser

---
+++ parser.go
@@ -0,0 +1,67 @@
+package paseto
+
+import (
+	"time"
+)
+
+type Parser struct {
+	rules []Rule
+}
+
+func NewV4Parser() Parser {
+	return Parser{[]Rule{ValidAt(time.Now())}}
+}
+
+func MakeParser(rules []Rule) Parser {
+	return Parser{rules}
+}
+
+func (p Parser) ParseV4Local(key V4SymmetricKey, tainted string, implicit []byte) (*Token, error) {
+	var message Message
+	var err error
+
+	if message, err = NewMessage(V4Local, tainted); err != nil {
+		return nil, err
+	}
+
+	var token *Token
+	if token, err = message.V4Decrypt(key, implicit); err != nil {
+		return nil, err
+	}
+
+	return p.validate(*token)
+}
+
+func (p Parser) ParseV4Public(key V4AsymmetricPublicKey, tainted string, implicit []byte) (*Token, error) {
+	var message Message
+	var err error
+
+	if message, err = NewMessage(V4Public, tainted); err != nil {
+		return nil, err
+	}
+
+	var token *Token
+	if token, err = message.V4Verify(key, implicit); err != nil {
+		return nil, err
+	}
+
+	return p.validate(*token)
+}
+
+func (p Parser) validate(token Token) (*Token, error) {
+	for _, rule := range p.rules {
+		if err := rule(token); err != nil {
+			return nil, err
+		}
+	}
+
+	return &token, nil
+}
+
+func (p *Parser) SetRules(rules []Rule) {
+	p.rules = rules
+}
+
+func (p *Parser) AddRule(rule ...Rule) {
+	p.rules = append(p.rules, rule...)
+}
